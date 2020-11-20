c12cb9fcba79bb79925e02c228cbd79d5344a15d
cammac <camm@rocktechcomputer.com>
2020-11-19 17:42:17 -0800

    cont AutoboxingChallenge,added Branch, Bank classes

--- AutoboxingChallenge/src/com/lambda/Customer.java
+++ AutoboxingChallenge/src/com/lambda/Customer.java
@@ -4,19 +4,20 @@ import java.util.ArrayList;
 
 public class Customer {
     private String name;
-    ArrayList<Double> transactions = new ArrayList<Double>();
+    private ArrayList<Double> transactions;
 
-    public Customer(String name, ArrayList<Double> transactions) {
+    public Customer(String name, double initialAmount) {
         this.name = name;
-        this.transactions = transactions;
+        this.transactions = new ArrayList<Double>();
+        addTransaction(initialAmount);
+    }
+
+    public void addTransaction(double amount) {
+        this.transactions.add(amount);
     }
 
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public ArrayList<Double> getTransactions() {
