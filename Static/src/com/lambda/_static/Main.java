f6bfd444dd6615c681a33c3227291ac76dad8f3e
cammac <camm@rocktechcomputer.com>
2020-12-21 09:15:42 -0800

    completed Static

--- Static/src/com/lambda/_static/Main.java
+++ Static/src/com/lambda/_static/Main.java
@@ -2,17 +2,26 @@ package com.lambda._static;
 
 public class Main {
 
+    public static int multiplier = 7;
+
     public static void main(String[] args) {
 
-        StaticTest firstInstance = new StaticTest("1st Instance");
-        System.out.println(firstInstance.getName() + " is instance number " + StaticTest.getNumInstances());
+//        StaticTest firstInstance = new StaticTest("1st Instance");
+//        System.out.println(firstInstance.getName() + " is instance number " + StaticTest.getNumInstances());
+//
+//        StaticTest secondInstance = new StaticTest("2nd Instance");
+//        System.out.println(secondInstance.getName() + " is instance number " + StaticTest.getNumInstances());
+//
+//        StaticTest thirdInstance = new StaticTest("3nd Instance");
+//        System.out.println(thirdInstance.getName() + " is instance number " + StaticTest.getNumInstances());
 
-        StaticTest secondInstance = new StaticTest("2nd Instance");
-        System.out.println(secondInstance.getName() + " is instance number " + StaticTest.getNumInstances());
+        int answer = multiply(6);
+        System.out.println("The answer is " + answer);
+        System.out.println("Multiplier is " + multiplier);
 
-        StaticTest thirdInstance = new StaticTest("3nd Instance");
-        System.out.println(thirdInstance.getName() + " is instance number " + StaticTest.getNumInstances());
+    }
 
-
+    public static int multiply(int number) {
+        return number * multiplier;
     }
 }
