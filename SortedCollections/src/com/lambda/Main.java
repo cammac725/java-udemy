1b6fc6d730c131779227f7a9a1dcf41479118257
cammac <camm@rocktechcomputer.com>
2021-01-14 15:05:59 -0800

    completed Collections challenge and section

---
+++ SortedCollections/src/com/lambda/Main.java
@@ -0,0 +1,151 @@
+package com.lambda;
+
+import java.util.Map;
+
+public class Main {
+
+    private static StockList stockList = new StockList();
+
+    public static void main(String[] args) {
+        StockItem temp = new StockItem("bread", 0.86, 100);
+	    stockList.addStock(temp);
+
+	    temp = new StockItem("cake", 1.10, 7);
+	    stockList.addStock(temp);
+
+        temp = new StockItem("car", 12.50, 2);
+        stockList.addStock(temp);
+
+        temp = new StockItem("chair", 62.0, 10);
+        stockList.addStock(temp);
+
+        temp = new StockItem("cup", .50, 200);
+        stockList.addStock(temp);
+
+        temp = new StockItem("cup", 0.45, 7);
+        stockList.addStock(temp);
+
+        temp = new StockItem("door", 72.95, 4);
+        stockList.addStock(temp);
+
+        temp = new StockItem("juice", 2.50, 36);
+        stockList.addStock(temp);
+
+        temp = new StockItem("phone", 96.99, 35);
+        stockList.addStock(temp);
+
+        temp = new StockItem("towel", 2.40, 80);
+        stockList.addStock(temp);
+
+        temp = new StockItem("vase", 8.76, 40);
+        stockList.addStock(temp);
+
+        System.out.println(stockList);
+
+        for (String s : stockList.Items().keySet()) {
+            System.out.println(s);
+        }
+
+        Basket camsBasket = new Basket("Cam");
+
+        sellItem(camsBasket, "car", 1);
+        System.out.println(camsBasket);
+
+        sellItem(camsBasket, "car", 1);
+        System.out.println(camsBasket);
+
+        if (sellItem(camsBasket, "car", 1) != 1) {
+            System.out.println("There are no more cars in stock");
+        }
+
+        sellItem(camsBasket, "a wrench", 5);
+
+        sellItem(camsBasket, "juice", 4);
+        sellItem(camsBasket, "cup", 12);
+        sellItem(camsBasket, "bread", 1);
+
+        Basket basket = new Basket("customer");
+        sellItem(basket, "cup", 100);
+        sellItem(basket, "juice", 5);
+        removeItem(basket, "cup", 1);
+        System.out.println(basket);
+
+        removeItem(camsBasket, "car", 1);
+        removeItem(camsBasket, "cup", 9);
+        removeItem(camsBasket, "car", 1);
+        System.out.println("cars removed: " + removeItem(camsBasket, "car", 1));
+
+        System.out.println(camsBasket);
+
+        removeItem(camsBasket, "bread", 1);
+        removeItem(camsBasket, "cup", 3);
+        removeItem(camsBasket, "juice", 4);
+        removeItem(camsBasket, "cup", 3);
+        System.out.println(camsBasket);
+
+        System.out.println("\ndisplay stock list before and after checkout");
+        System.out.println(basket);
+        System.out.println(stockList);
+        checkOut(basket);
+        System.out.println(basket);
+        System.out.println(stockList);
+
+
+        // can't add items to stockList this way, need to use methods we set up
+//        temp = new StockItem("pen", 1.12);
+//        stockList.Items().put(temp.getName(), temp);
+
+        StockItem car = stockList.Items().get("car");
+        if (car != null) {
+            car.adjustStock(2000);
+        }
+        if (car != null) {
+            stockList.get("car").adjustStock(-1000);
+        }
+
+        System.out.println(stockList);
+
+        System.out.println();
+//        for (Map.Entry<String, Double> price : stockList.PriceList().entrySet()) {
+//            System.out.println(price.getKey() + " costs " + price.getValue());
+//        }
+
+        checkOut(camsBasket);
+        System.out.println(camsBasket);
+
+    }
+
+    public static int sellItem(Basket basket, String item, int quantity) {
+        // retrieve the item from stock list
+        StockItem stockItem = stockList.get(item);
+        if (stockItem == null) {
+            System.out.println("We don't sell " + item);
+            return 0;
+        }
+        if (stockList.reserveStock(item, quantity) != 0) {
+            return basket.addToBasket(stockItem, quantity);
+        }
+        return 0;
+    }
+
+    public static int removeItem(Basket basket, String item, int quantity) {
+        // retrieve the item from stock list
+        StockItem stockItem = stockList.get(item);
+        if (stockItem == null) {
+            System.out.println("We don't sell " + item);
+            return 0;
+        }
+        if (basket.removeFromBasket(stockItem, quantity) == quantity) {
+            return stockList.unreserveStock(item, quantity);
+        }
+        return 0;
+    }
+
+    public static void checkOut(Basket basket) {
+        for (Map.Entry<StockItem, Integer> item : basket.Items().entrySet()) {
+            stockList.sellStock(item.getKey().getName(), item.getValue());
+        }
+        basket.clearBasket();
+    }
+
+}
---
+++ SortedCollections/src/com/lambda/MainChallenge.java
@@ -0,0 +1,7 @@
+package com.lambda;
+
+public class MainChallenge {
+
+
+
+}
